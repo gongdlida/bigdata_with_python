3f4ccf61b3313c8be4684a8963202f1ef87606e6
jun931223 <jun931223@gmail.com>
2022-04-09 15:36:49 +0900

    til: nltk token

---
+++ NLTK_token.py
@@ -0,0 +1,21 @@
+ import nltk
+ nltk.download('averaged_perceptron_tagger')
+
+#문장 토큰화
+from nltk.tokenize import sent_tokenize
+text = "The most beautiful things in the world cannot be seen or touched. They are felt with the heart."
+text.lower()
+tokenized_sentences = sent_tokenize(text)
+print(tokenized_sentences)
+
+#단어 토큰화
+from nltk.tokenize import word_tokenize
+text = "The most beautiful things in the world cannot be seen or touched. They are felt with the heart."
+tokenized_words = word_tokenize(text)
+print(tokenized_words)
+
+
+#품사태깅
+from nltk.tag import pos_tag
+tagged_words = pos_tag(tokenized_words)
+print(tagged_words)
